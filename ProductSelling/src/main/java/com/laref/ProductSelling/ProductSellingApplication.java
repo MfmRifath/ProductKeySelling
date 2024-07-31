a4c220ca3ccd7ba152ed713b4f05f1faa910a8cb
MfmRifath <mmfmrifath@gmail.com>
2024-07-31 11:25:47 +0530

    StartingProject

---
+++ ProductSelling/src/main/java/com/laref/ProductSelling/ProductSellingApplication.java
@@ -0,0 +1,13 @@
+package com.laref.ProductSelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSellingApplication.class, args);
+	}
+
+}
---
+++ ProductSelling/src/test/java/com/laref/ProductSelling/ProductSellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laref.ProductSelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
